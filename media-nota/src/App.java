0d2d74b109c11a5a901b5bc52185301700b72092
henrymcruz <henrymcruz04@gmail.com>
2025-02-05 15:57:56 -0300

    usando condição ternária v2

--- media-nota/src/App.java
+++ media-nota/src/App.java
@@ -1,8 +1,8 @@
 public class App {
     public static void main(String[] args) {
-        int nota = 5;
+        int nota = 8;
 
-        String resultado = nota >=7 ? "Aprovado" : "Reprovado";
+        String resultado = nota >=7 ? "Aprovado" : nota >=5 && nota <7 ? "Recuperação" : "Reprovado";
         System.out.println(resultado);
     }
 }
