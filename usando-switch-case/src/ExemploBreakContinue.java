4520f7440adfa501562e4268e8527e61f3f8beaa
henrymcruz <henrymcruz04@gmail.com>
2025-02-06 11:43:02 -0300

    teste numero impar

--- usando-switch-case/src/ExemploBreakContinue.java
+++ usando-switch-case/src/ExemploBreakContinue.java
@@ -1,9 +1,8 @@
 public class ExemploBreakContinue {
     public static void main(String[] args) {
-        for(int numero = 1; numero <=5; numero ++){
-            if(numero == 3)
-                continue;
-            System.out.println(numero);
+        for(int numero = 0; numero <=100; numero ++){
+            if(numero % 2 == 0)
+                System.out.println(numero);
         }
     }
 }
