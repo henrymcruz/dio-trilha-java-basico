a19e8f1e4fc3d63e2d2d70d69de4c3a84f809c6c
henrymcruz <henrymcruz04@gmail.com>
2025-02-06 11:25:30 -0300

    usando for each

--- usando-switch-case/src/ExemploForArrays.java
+++ usando-switch-case/src/ExemploForArrays.java
@@ -2,8 +2,8 @@ public class ExemploForArrays {
     public static void main(String[] args) {
         String alunos [] = {"João", "Felipe", "Julia", "Marcos"};
 
-        for(int x=0; x<alunos.length; x++){
-            System.out.println("O aluno no indice X = " + x + " é " + alunos[ x ]);
+        for(String aluno : alunos){
+            System.out.println("Nome do aluno é: " + aluno);
+            }
         }
     }
-}
