ffbc96823d5303f92791f4513311057a09959c8e
henrymcruz <henrymcruz04@gmail.com>
2025-02-06 11:44:47 -0300

    teste ok

--- usando-switch-case/src/ExemploBreakContinue.java
+++ usando-switch-case/src/ExemploBreakContinue.java
@@ -2,7 +2,9 @@ public class ExemploBreakContinue {
     public static void main(String[] args) {
         for(int numero = 0; numero <=100; numero ++){
             if(numero % 2 == 0)
-                System.out.println(numero);
+                System.out.println(numero+ " é Par");
+            else
+                System.out.println(numero + " é Impar");
         }
     }
 }
