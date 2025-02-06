3b26189d63f73b9392b48319902abcf6580a2cbb
henrymcruz <henrymcruz04@gmail.com>
2025-02-06 11:13:43 -0300

    testando FOR

---
+++ usando-switch-case/src/ExemploFor.java
@@ -0,0 +1,9 @@
+public class ExemploFor {
+    public static void main(String[] args) {
+        for(int carneirinhos = 1; carneirinhos <= 20; carneirinhos++){
+            // variavelContador--- verificação se for menor que 20--adiciona +1 ao contador
+            System.out.println("Contando Carineirinhos " + carneirinhos);
+        }
+        System.out.println("Joaozinho dormiu!");
+    }
+}
